525a029fab9e6c0f6acb7abc4e9acf91d91955cf
Zoltan Czako <czzo@erni.ro>
2019-09-06 10:44:09 +0300

    Pagination component

---
+++ PaginationComponent/Helpers/PaginationHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace BlazorPaginationComponent.Helpers
+{
+    public static class PaginationHelper
+    {
+        private const string DisabledClassName = "disabled";
+
+        public static int CalculateNumberOfPages(int totalItems, int pageSize)
+        {
+            return (int)Math.Ceiling(totalItems / (decimal)pageSize);
+        }
+
+        public static int GetPreviousPage(int currentPage)
+        {
+            return currentPage > 1 ? currentPage - 1 : currentPage;
+        }
+
+        public static int GetNextPage(int currentPage, int lastPage)
+        {
+            return currentPage < lastPage ? currentPage + 1 : currentPage;
+        }
+
+        public static string DisableForwardNavigation(int currentPage, int lastPage)
+        {
+            return currentPage >= lastPage ? DisabledClassName : string.Empty;
+        }
+
+        public static string DisableBackwardNavigation(int currentPage)
+        {
+            return currentPage <= 1 ? DisabledClassName : string.Empty;
+        }
+    }
+}
---
+++ PaginationComponent/PaginationComponent.razor.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Components;
+using BlazorPaginationComponent.Helpers;
+
+namespace BlazorPaginationComponent
+{
+    public class PaginationComponentCode : ComponentBase
+    {
+        [Parameter] public int TotalItems { get; set; }
+        [Parameter] public int CurrentPage { get; set; }
+        [Parameter] public int PageSize { get; set; }
+        [Parameter] public EventCallback<int> OnPageChange { get; set; }
+        [Parameter] public string CustomClass { get; set; }
+
+        public int LastPage { get; set; }
+
+        protected override void OnInitialized()
+        {
+            LastPage = PaginationHelper.CalculateNumberOfPages(TotalItems, PageSize);
+        }
+
+        public string DisableBackwardNavigation()
+        {
+            return PaginationHelper.DisableBackwardNavigation(CurrentPage);
+        }
+
+        public string DisableForwardNavigation()
+        {
+            return PaginationHelper.DisableForwardNavigation(CurrentPage, LastPage);
+        }
+
+        public int GetNextPage()
+        {
+            return PaginationHelper.GetNextPage(CurrentPage, LastPage);
+        }
+
+        public int GetPreviousPage()
+        {
+            return PaginationHelper.GetPreviousPage(CurrentPage);
+        }
+    }
+}
